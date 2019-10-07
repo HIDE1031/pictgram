66f4f16808edceaaa734409124909eaa7a315894
HIDE <sthd1031@gmail.com>
2019-10-07 08:28:44 +0000

    practice

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to root_path, success: '登録が完了しました'
+    else
+      flash.now[:danger] = "登録に失敗しました"
+      render :new
+    end
+  end
+  
+  private
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  end
+end
